2eb495271686b04c1afc352495ce23a825450279
Cezar Detilio <cezardetilio@gmail.com>
2022-07-02 07:08:24 -0300

    H2 database, test profile, JPA

--- src/main/java/com/cezar/projectspring/entities/Usuario.java
+++ src/main/java/com/cezar/projectspring/entities/Usuario.java
@@ -2,20 +2,30 @@ package com.cezar.projectspring.entities;
 
 import java.io.Serializable;
 
-public class User implements Serializable{
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class Usuario implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
 	private String phone;
 	private String password;
 	
-	public User() {
+	public Usuario() {
 		
 	}
 
-	public User(Long id, String name, String email, String phone, String password) {
+	public Usuario(Long id, String name, String email, String phone, String password) {
 		super();
 		this.id = id;
 		this.name = name;
@@ -80,7 +90,7 @@ public class User implements Serializable{
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		User other = (User) obj;
+		Usuario other = (Usuario) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
